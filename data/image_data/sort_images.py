6a5fbca85d746b7b830ca0a1f62fb0a3d2912c8d
SavannaMoss <60207230+SavannaMoss@users.noreply.github.com>
2020-11-13 15:44:24 -0500

    Sorted images into train and test sets.

--- data/image_data/sort_images.py
+++ data/image_data/sort_images.py
@@ -4,25 +4,25 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def main():
-    # import pdb; pdb.set_trace()
-
+    
     # load text file
     train_file_names = "train_file_names.txt"
     test_file_names = "test_file_names.txt"
     train = np.loadtxt(train_file_names, str, delimiter="/n")
     test = np.loadtxt(test_file_names, str, delimiter="/n")
 
-    dst_train = dir_path+"\\train_images"
-    dst_test = dir_path+"\\test_images"
+    # src = "C:/Users/savan/Documents/GitHub/BroadStrokes/data/image_data/"
+    # dst_train = "C:/Users/savan/Documents/GitHub/BroadStrokes/data/image_data/train_images/"
+    # dst_test = "C:/Users/savan/Documents/GitHub/BroadStrokes/data/image_data/test_images/"
 
-    search through folder and compare file name to train and test
-    for root, dirs, files in os.walk(dir_path):
-        for file in files:
-            if (file.endswith('.jpg')) and (file in train):
-                os.rename(dir_path, dst_train)
-
-            if (file.endswith('.jpg')) and (file in test):
-                os.rename(dir_path, dst_test)
+    # # search through folder and compare file name to train and test
+    # for root, dirs, files in os.walk(dir_path):
+    #     for file in files:
+    #         if (file.endswith('.jpg')) and (file in train):
+    #             os.rename(src+file, dst_train+file)
+    #
+    #         if (file.endswith('.jpg')) and (file in test):
+    #             os.rename(src+file, dst_test+file)
 
 if __name__ == '__main__':
     main()
