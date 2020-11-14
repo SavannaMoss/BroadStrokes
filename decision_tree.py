5b4caec90384b6141a0d41fe2acbf821a0a2ae9f
Hannahbw12 <hannah.wilberdingbw@gmail.com>
2020-11-13 19:32:20 -0500

    Update decision_tree.py
    Model now working but error on attributes for plot_tree() in regards to size of the attributes data
    
    Co-Authored-By: Savanna Moss <60207230+SavannaMoss@users.noreply.github.com>

--- decision_tree.py
+++ decision_tree.py
@@ -6,8 +6,9 @@ import numpy as np
 import os
 import pdb
 from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.metrics import confusion_matrix
+from sklearn.impute import SimpleImputer
 
 ROOT = os.path.dirname(os.path.abspath(__file__))  # root directory of this code
 
@@ -42,26 +43,34 @@ def main():
     classes = np.loadtxt(classesfile, str, delimiter=",")
 
     # Train a decision tree via information gain on the training data
-
     # USE ONEHOT ENCODING FOR CATEGORICAL ATTRIBUTES
-    encTrain = OneHotEncoder(handle_unknown='ignore')
-    encTrain.fit(traindata)
-    encTrain = encTrain.transform(traindata).toarray().astype('float32')
+    enc = OneHotEncoder(handle_unknown='ignore').fit(traindata[:, 1:3])
+    onehot_train = enc.transform(traindata[:, 1:3])
+    onehot_test = enc.transform(testdata[:, 1:3])
 
+    #changes empty strings to np.nan
+    traindata[traindata[:,0]=='',0] = np.nan
+    testdata[testdata[:,0]=='',0] = np.nan
 
-    encTest = OneHotEncoder(handle_unknown='ignore')
-    encTest.fit(traindata)
-    encTest = encTest.transform(testdata).toarray().astype('float32')
+    #changes np.nan to median of dates
+    X = SimpleImputer(strategy = 'median').fit_transform(traindata[:,0].reshape(-1,1))
+    Xt = SimpleImputer(strategy = 'median').fit_transform(testdata[:,0].reshape(-1,1))
 
+    #concatenate the onehots and the dates
+    X = np.concatenate((X, onehot_train.toarray()), axis=1).astype('float32')
+    Xt = np.concatenate((Xt, onehot_test.toarray()), axis=1).astype('float32')
+
+    #encodes the labels
+    Y = LabelEncoder().fit_transform(trainlabels)
+    Yt = LabelEncoder().fit_transform(testlabels)
 
 
     # create the decision tree
     tree = DecisionTreeClassifier(criterion="entropy", random_state=0)
-    tree.fit(encTrain, trainlabels)
-    # pdb.set_trace()
-    
+    tree.fit(X, Y)
+
     #Test the decision tree
-    pred = tree.predict(encTest)#, check_input = False)
+    pred = tree.predict(Xt, check_input = False)
 
     # # Show the confusion matrix for test data
     # cm = confusion_matrix(testlabels, pred)
@@ -69,15 +78,15 @@ def main():
     # print(cm)
 
     # Compare training and test accuracy
-    trainaccuracy = tree.score(encTrain, trainlabels)
+    trainaccuracy = tree.score(X, Y)
     print("Training accuracy:", trainaccuracy)
 
-    testaccuracy = tree.score(encTest, testlabels)
+    testaccuracy = tree.score(Xt, Yt)
     print("Testing accuracy:", testaccuracy)
 
     # Visualize the tree using matplotlib and plot_tree
     fig = plt.figure(figsize=(13,8))
-    fig = plot_tree(tree, feature_names=attributes, class_names= classes, filled=True, rounded=True)
+    fig = plot_tree(tree, class_names= classes, filled=True, rounded=True)
     plt.show()
 
 if __name__ == '__main__':
