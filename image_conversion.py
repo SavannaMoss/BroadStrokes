ada687883f47b19f95f6ba17a9b973ddd040d259
SavannaMoss <60207230+SavannaMoss@users.noreply.github.com>
2020-12-10 21:11:53 -0500

    Fixed image_conversion.py
    Now correctly removes extra comma at the end of the file.

--- image_conversion.py
+++ image_conversion.py
@@ -8,10 +8,9 @@ import os
 import tensorflow as tf
 tf.random.set_seed(3520)
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+DATAPATH = os.path.realpath('data/image_data')
 
 def main():
-    # Load images and labels
     print("Loading data...")
 
     x = loaddata('train')
@@ -21,48 +20,63 @@ def main():
     print("Test data file created!")
 
 def loaddata(mode):
-    '''Load data and labels.'''
-    sz = 224
+    sz = 224 # new image size / number of total pixels
 
+    # find correct images
     if mode == 'train':
-        img_path = os.path.realpath("data/image_data/train_resized/")
+        img_path = DATAPATH+"\\train_resized"
         f = open("train_data.txt", "a")
+
     elif mode == 'test':
-        img_path = os.path.realpath("data/image_data/test_resized/")
+        img_path = DATAPATH+"\\test_resized"
         f = open("test_data.txt", "a")
     else:
         print("Unrecognized mode.")
 
+    # obtain file names
+    files = [f for f in os.listdir(img_path)]
+
+    # loop through files and convert to array
     count = 0
-    for root, dirs, files in os.walk(img_path):
-        for file in files:
-            if (file.endswith('.png')):
+    for file in files:
+        if (file.endswith('.png')):
 
-                img = tf.keras.preprocessing.image.load_img(
-                    img_path+"\\"+file,
-                    color_mode="grayscale",
-                    target_size=(sz, sz),
-                    interpolation='bilinear')
-                img_array = tf.keras.preprocessing.image.img_to_array(img)
-                img_array = img_array / 255
+            img = tf.keras.preprocessing.image.load_img(
+                img_path+"\\"+file,
+                color_mode="grayscale",
+                target_size=(sz, sz),
+                interpolation='bilinear')
+            img_array = tf.keras.preprocessing.image.img_to_array(img)
+            img_array = img_array / 255
 
-                img_array = np.array(img_array).flatten()
+            img_array = np.array(img_array).flatten()
 
-                # import pdb; pdb.set_trace()
-                np.savetxt(f, img_array, newline=",")
-                f.seek(f.tell() - 2,  os.SEEK_SET)
-                f.truncate()
+            # write image array to file
+            np.savetxt(f, img_array, newline=",")
 
-                f.write("\n")
-                #print("printed new line")
-            
-                count += 1
-                print(count)
-            
-            if count == 3:
-                break
+            f.write("\n")
 
+            break # temp while testing, remove when using on all data
 
+    # close file
+    f.close()
+
+    # remove last comma at the end of the file
+    if mode == 'train':
+        with open("train_data.txt", 'rb+') as f:
+            f.seek(0, os.SEEK_END)
+            f.seek(f.tell() - 3, os.SEEK_SET)
+            f.truncate()
+
+    elif mode == 'test':
+        with open("test_data.txt", 'rb+') as f:
+            f.seek(0, os.SEEK_END)
+            f.seek(f.tell() - 3, os.SEEK_SET)
+            f.truncate()
+    else:
+        print("Unrecognized mode.")
+
+    # close file
     f.close()
 
 if __name__ == '__main__':
