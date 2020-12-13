6e5532970a09ad6dd643c61b57cf550569c9962d
Hannahbw12 <hannah.wilberdingbw@gmail.com>
2020-12-12 19:50:14 -0500

    Testing dim_reduction
    testing pca, lda, and identity with differen number of components

--- KNN.py
+++ KNN.py
@@ -6,15 +6,19 @@ import os
 import matplotlib.pyplot as plt
 
 # sci-kit imports
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier, NeighborhoodComponentsAnalysis
 from skimage.util import montage
 from sklearn.metrics import precision_score, confusion_matrix
+
+#PCA for dimention reduction
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 # custom library
 from load_data import load_data
 
 def main():
-
+    n_neighbors = 28
     print("Loading training and testing data...")
     (xtrain, ttrain), (xtest, ttest) = load_data()
 
@@ -24,13 +28,26 @@ def main():
     Testing Data: xtest
     Testing Labels: ttest
     '''
+    dim_reduction = NeighborhoodComponentsAnalysis(n_components = 64, init = 'pca') #it will choose beseet one pca, lda,identity, random, nparray
 
     print("Training KNN model...")
-    clf = KNeighborsClassifier(26, weights='distance')
-    clf.fit(xtrain, ttrain)
+    clf = KNeighborsClassifier(n_neighbors, weights='distance')
+    #clf.fit(xtrain, ttrain)
+
+
+    dim_reduction.fit(xtrain, ttrain)
+    clf.fit(dim_reduction.transform(xtrain), ttrain)
+    # print("Training Accuracy", name, ": ", clf.score(model.trainsform(xtrain), ttrain))
+    print("Testing Accuracy: ", clf.score(dim_reduction.transform(xtest), ttest))
+
+
+
+
 
     # compute performance metrics
-    print("Testing Accuracy:", clf.score(xtest, ttest))
+    # print("Training Accuracy:", clf.score(xtrain, ttrain))
+    # print("Testing Accuracy:", clf.score(xtest, ttest))
+
 
     # pred = clf.predict(xtest)
     # print("Precision:", precision_score(ttest, pred, average='micro'))
