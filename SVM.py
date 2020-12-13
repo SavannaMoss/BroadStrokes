9b205369e56ae39533469852c2dec5009d6d57f5
SavannaMoss <60207230+SavannaMoss@users.noreply.github.com>
2020-12-13 07:38:13 -0500

    Added confusion matrix plot

--- SVM.py
+++ SVM.py
@@ -9,12 +9,14 @@ import matplotlib.pyplot as plt
 from sklearn import svm
 from skimage.feature import hog
 from skimage.util import montage
-from sklearn.metrics import precision_score, confusion_matrix
+from sklearn.metrics import precision_score, plot_confusion_matrix
 
 # custom library
 from load_data import load_data
 
 def main():
+    np.random.seed(0)
+
     print("Loading training and testing data...")
     (xtrain, ttrain), (xtest, ttest) = load_data()
 
@@ -28,26 +30,37 @@ def main():
     Testing Labels: ttest
     '''
 
-    print("Extracting HOG Features...")
+    print("Extracting HOG features...")
     x = getHOG(xtrain)
     xt = getHOG(xtest)
 
     print("Setting up labels...")
     # converts categorical class labels to unique values
     _, t = np.unique(ttrain, return_inverse=True)
-    _, tt = np.unique(ttest, return_inverse=True)
+    labels, tt = np.unique(ttest, return_inverse=True)
 
     print("Training SVM...")
-    np.random.seed(0)
-    clf = svm.SVC(C=1.0, kernel='linear')
+    clf = svm.SVC(C=1.0)
     clf.fit(x, t)
 
     # performance metrics
     print("Testing Accuracy:", clf.score(xt, tt))
 
-    # pred = clf.predict(xt)
-    # print("Precision:", precision_score(tt, pred, average='weighted'))
-    # print("Confusion Matrix:\n", confusion_matrix(tt, pred))
+    print("Precision:", precision_score(tt, clf.predict(xt), average='micro'))
+
+    plt.rc('font', size=6)
+    plt.rc('figure', titlesize=10)
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    plt.subplots_adjust(bottom=0.2, top=0.9, right=0.9, left=0.1)
+    ax.set_title("SVM Confusion Matrix")
+    cm = plot_confusion_matrix(clf, xt, tt,
+                                normalize='all',
+                                display_labels=labels,
+                                xticks_rotation='vertical',
+                                cmap=plt.cm.Blues,
+                                ax=ax)
+    plt.show()
 
 def getHOG(data):
     x = []
