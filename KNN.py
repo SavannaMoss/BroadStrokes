de29a0b967316f435d809dbcee1a11fe2f921117
Hannahbw12 <hannah.wilberdingbw@gmail.com>
2020-12-13 02:50:42 -0500

    working KNN
    scatter plot workig, but no key yet

--- KNN.py
+++ KNN.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # sci-kit imports
 from sklearn.neighbors import KNeighborsClassifier, NeighborhoodComponentsAnalysis
 from skimage.util import montage
-from sklearn.metrics import precision_score, confusion_matrix
+from sklearn.metrics import precision_score, confusion_matrix, ConfusionMatrixDisplay
 
 #PCA for dimention reduction
 from sklearn.decomposition import PCA
@@ -28,7 +28,7 @@ def main():
     Testing Data: xtest
     Testing Labels: ttest
     '''
-    dim_reduction = NeighborhoodComponentsAnalysis(n_components = 64, init = 'pca') #it will choose beseet one pca, lda,identity, random, nparray
+    dim_reduction = NeighborhoodComponentsAnalysis(n_components = 80, init = 'pca', random_state = 0) #it will choose beseet one pca, lda,identity, random, nparray
 
     print("Training KNN model...")
     clf = KNeighborsClassifier(n_neighbors, weights='distance')
@@ -41,7 +41,17 @@ def main():
     print("Testing Accuracy: ", clf.score(dim_reduction.transform(xtest), ttest))
 
 
+    #imaging
+    transformed = dim_reduction.transform(xtest)
+    labels, num = np.unique(ttest, return_inverse = True)
+    fig, ax = plt.subplots()
+    # legend1 =  ax.legend((labels[0], labels[1], labels[2],labels[3],labels[4],labels[5],labels[6],labels[7],labels[8],labels[9]),  loc = "upper right", title = "Artists")
+    legend1 = ax.legend(labels.legend_elements(num = 10), loc = "upper right", title = "Artists")
+    ax.add_artist(legend1)
+    ax.scatter(transformed[:,0], transformed[:,1], c = num, cmap = 'tab10')
 
+    # ax.legend((labels[0], labels[1], labels[1],labels[1],labels[1],labels[1],labels[1],labels[1],labels[1],labels[1],labels[1],))
+    plt.show()
 
 
     # compute performance metrics
